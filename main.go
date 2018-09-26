8c58f50e9af2c581fa87944352c287e3f4c47389
sylendran <sylendra@ardhika.com>
2018-09-26 16:16:08 +0530

    changes made for deploy

--- main.go
+++ main.go
@@ -1,11 +1,32 @@
+/*
+Copyright IBM Corp. 2016 All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+		 http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package main
+
+//WARNING - this chaincode's ID is hard-coded in chaincode_example04 to illustrate one way of
+//calling chaincode from a chaincode. If this example is modified, chaincode_example04.go has
+//to be modified as well with the new ID of chaincode_example02.
+//chaincode_example05 show's how chaincode ID can be passed in as a parameter instead of
+//hard-coding.
 
 import (
 	"fmt"
-	"log"
 	"strconv"
 
-	shim "github.com/hyperledger/fabric/core/chaincode/shim"
+	"github.com/hyperledger/fabric/core/chaincode/shim"
 	pb "github.com/hyperledger/fabric/protos/peer"
 )
 
@@ -45,10 +66,8 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
 
 	err = stub.PutState(B, []byte(strconv.Itoa(Bval)))
 	if err != nil {
-		log.Println("Error in put state : ", err.Error())
 		return shim.Error(err.Error())
 	}
-	log.Println("success in put state")
 
 	return shim.Success(nil)
 }
